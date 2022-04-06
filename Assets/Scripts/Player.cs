9e0347c3776660a8866d1cd0ddf0841eb45bfc14
Kealstex <kealstex@yandex.com>
2022-04-06 22:23:49 +0300

    change input actions behaviour to "Invoke Unity Events"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,36 @@ using Vector3 = UnityEngine.Vector3;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float speed;
-    private Vector3 _direction;
+    private float moveSpeed;
+    private Vector2 m_Move;
     
     // Update is called once per frame
     public void Update()
     {
-        transform.Translate(_direction);
-        var x = Mathf.Clamp(transform.position.x, -9f, 9f);
-        var y = Mathf.Clamp(transform.position.y, -3.7f, 0f);
-        transform.position = new Vector3(x, y, 0f);
+        Move(m_Move);
+        PositionClamp();
     }
 
-    public void OnMove(InputValue input)
+    public void OnMove(InputAction.CallbackContext input)
     {
-        var inputVec = input.Get<Vector2>() * speed * Time.deltaTime;
-        _direction = new Vector3(inputVec.x, inputVec.y,0 );
+        m_Move = input.ReadValue<Vector2>();
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if(direction.sqrMagnitude < 0.1f)
+            return;
+        var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+        var move = direction * scaledMoveSpeed;
+        transform.position += new Vector3(move.x, move.y, 0);
+    }
+
+    private void PositionClamp()
+    {
+        var position = transform.position;
+        var x = Mathf.Clamp(position.x, -9f, 9f);
+        var y = Mathf.Clamp(position.y, -3.7f, 0f);
+        position = new Vector3(x, y, 0f);
+        transform.position = position;
     }
 }
