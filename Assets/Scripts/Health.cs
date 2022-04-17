abbac97dae01d92ca073aa73df52e9e370c76bb0
Kealstex <kealstex@yandex.com>
2022-04-17 16:03:25 +0300

    Add assets from cource ressurces, add stop enemy spawn -- stop enemy spawn when player is dead (Health+SpawnManager update)

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,12 +3,16 @@
 public class Health : MonoBehaviour
 {
     [SerializeField] private int value;
+    [SerializeField] private SpawnManager spawnManager;
 
     public void Damage(int damageValue){
-        value -= damageValue;
         
-        if(IsDeath())
+        value -= damageValue;
+
+        if (IsDeath()){
+            spawnManager.OnPlayerDeath();
             Destroy(gameObject);
+        }
     }
 
     private bool IsDeath(){
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,15 +6,23 @@ using Random = UnityEngine.Random;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject enemyContainer;
+    private bool _stopSpawn = false;
+
     private void Start(){
         StartCoroutine(SpawnRoutine());
     }
 
+    public void OnPlayerDeath(){
+        _stopSpawn = true;
+    }
+
     private IEnumerator SpawnRoutine(){
-        for (;;){
+        while (_stopSpawn){
             var randomX = Random.Range(-10f, 10f);
-            var position = new Vector3(randomX, 7f,0f);
-            var newEnemy= Instantiate(enemy, position, Quaternion.identity);
+            var position = new Vector3(randomX, 7f, 0f);
+            Instantiate(enemy, position, Quaternion.identity, enemyContainer.transform);
+            
             yield return new WaitForSeconds(5f);
         }
     }
