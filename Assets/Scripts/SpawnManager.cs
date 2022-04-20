183c0e00ee6513b8cca4b42230fc1846f1dba9c1
Kealstex <kealstex@yandex.com>
2022-04-20 23:10:54 +0300

    add Spawn Triple Shot powerup, add folders for powerup+enemy prefabs and powerup animation folder

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,28 +1,50 @@
 ﻿using System;
 using System.Collections;
+using System.Linq.Expressions;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject tripleShotPowerUp;
     [SerializeField] private GameObject enemyContainer;
-    [SerializeField] private float spawnTime;
+    [SerializeField] private float enemySpawnTime; 
     private bool _stopSpawn = false;
+    
+    private float tripleShotSpawnTime = 7f;
+    
+    private float _maxYScreenPosition = 7f;
+    private float _minXScreenPosition = -10f;
+    private float _maxXScreenPosition = 10f;
 
     private void Start(){
-        StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnObjectRoutine(enemy, enemySpawnTime, enemyContainer));
+        StartCoroutine(SpawnObjectRoutine(tripleShotPowerUp, tripleShotSpawnTime));
     }
 
     public void OnPlayerDeath(){
         _stopSpawn = true;
     }
 
-    private IEnumerator SpawnRoutine(){
+    private Vector2 RandomTopSpawn(){
+        var randomX = Random.Range(_minXScreenPosition, _maxXScreenPosition);
+        return new Vector2(randomX, _maxYScreenPosition);
+    }
+
+    private IEnumerator SpawnObjectRoutine(GameObject obj, float spawnTime = 0f, GameObject parent = null ){
         while (!_stopSpawn){
-            var randomX = Random.Range(-10f, 10f);
-            var position = new Vector3(randomX, 7f, 0f);
-            Instantiate(enemy, position, Quaternion.identity, enemyContainer.transform);
+            var position = RandomTopSpawn();
+            
+            var spawnObj = Instantiate(obj, position, Quaternion.identity);
+            
+            if (parent != null){
+                spawnObj.transform.SetParent(parent.transform);
+            }
+
+            if (spawnTime == 0f){
+                spawnTime = Random.Range(5f, 12f);
+            }
             
             yield return new WaitForSeconds(spawnTime);
         }
